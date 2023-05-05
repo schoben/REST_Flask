91f64c54a92b5844683b22d7fb9642215ece8b67
Avner Duchovni <avnerd@checkpoint.com>
2023-05-05 16:32:23 +0300

    Initial commit. Contains a working version of a simple Flask server supporting posting and getting of dishes. Includes also a test file to use with the server. There is also a TODO file with what still needs to be done.

---
+++ svr.py
@@ -0,0 +1,153 @@
+"""
+Exercise 1# for Cloud Computing course
+Creating a server docker image implementing a REST API
+"""
+
+from flask import Flask  # , jsonify
+from flask_restful import Resource, Api, reqparse
+
+
+app = Flask(__name__)  # initialize Flask
+api = Api(app)  # create API
+
+
+# TODO: Move these two classes to a separate module (decouple)
+class Dish:
+    """Class representing a single dish"""
+
+    def __init__(self, name, idx):
+        self.idx = idx
+        self.name = name
+        # TODO: Use ninja API to get and set the variables (cal, size, sodium, sugar). Raise an exception if needed
+
+    def _get_idx(self):
+        return self.idx
+
+    def _get_name(self):  # TODO: Use property instead of getter
+        return self.name
+
+    def get_as_dict(self):
+        return {'name': self.name, 'id': self.idx, 'cal': 10, 'size': 10, 'sodium': 10, 'sugar': 10}  # TODO
+
+
+class DishesCollection:
+    """Collection class of all the dishes"""
+
+    def __init__(self):
+        self.dishes = dict()
+        self.num_of_dishes = 0
+
+    def _get_all_dishes(self):
+        return {idx: dish.get_as_dict() for idx, dish in self.dishes.items()}
+
+    def get_dish_by_idx(self, idx):
+        return self.dishes[idx]
+
+    def get_dish_by_name(self, name):
+        for _, dish in self.dishes.items():  # Use .values() instead
+            if dish._get_name() == name:
+                return dish
+        raise ValueError(f"Couldn't find a dish that goes by the name {name}")
+
+    def dish_exists(self, name):
+        try:
+            self.get_dish_by_name(name)
+            return True
+        except ValueError:
+            return False
+
+    def add_dish(self, name):
+        self.num_of_dishes += 1  # No support for concurrency
+        print(f"Adding to Dishes: name {name}, idx: {self.num_of_dishes}")
+        dish = Dish(name, self.num_of_dishes)
+        self.dishes[self.num_of_dishes] = dish
+        return self.num_of_dishes
+
+    def delete_dish_by_idx(self, idx):
+        dish = self.dishes.pop(idx)  # removing the dish from the DishesCollection
+        del dish  # removing the deleted dish from memory
+
+    def get_dish_idx_by_name(self, name):
+        dish = self.get_dish_by_name(name)
+        return dish._get_idx()
+
+
+dishes_collection = DishesCollection()  # Global dishes collection
+
+
+class Dishes(Resource):
+    """A Class implementing a REST API for dealing with Dishes"""
+
+    def get(self):
+        return dishes_collection._get_all_dishes(), 200
+
+    def post(self):
+        # TODO: verify the JSON header. return 0 and status 415 (slide 13)
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', location='args', required=True)  # location='args' is required, at least on macOS
+        args = parser.parse_args()
+        try:
+            dish_name = args['name']
+        except KeyError:
+            return -1, 422  # No dish name supplied
+        if dishes_collection.dish_exists(dish_name):
+            return -2, 422  # Dish name already exists
+        try:
+            idx = dishes_collection.add_dish(dish_name)
+        except Exception:  # Split to two types of exceptions
+            # Not recognized: return -3, status 400 (slide 15)
+            # Unreachable: return -4, status 504
+            raise NotImplementedError("Unexpected error (ninja?)")
+        return idx, 201
+
+    def delete(self):  # Can we simply remove this and get the same functionality?
+        return "This method is not allowed for the requested URL", 405
+
+
+
+class DishesId(Resource):
+    """RESTful API for dealing with dishes/id resources"""
+
+    def get(self, idx):
+        try:
+            return dishes_collection.get_dish_by_idx(idx).get_as_dict(), 200
+        except KeyError:
+            return -5, 404
+
+    def delete(self, idx):
+        try:
+            dishes_collection.delete_dish_by_idx(idx)
+            return idx, 200
+        except KeyError:
+            return  -5, 404
+
+
+class DishesName(Resource):
+    """RESTful API for dealing with dishes/name resources"""
+
+    def get(self, name):
+        try:
+            return dishes_collection.get_dish_by_name(name).get_as_dict()  # TODO: Add response value
+        except ValueError as e:
+            print(str(e))
+            return -5, 404
+
+    def delete(self, name):
+        try:
+            dish = dishes_collection.get_dish_by_name(name)
+            idx = dish._get_idx()
+            dishes_collection.delete_dish_by_idx(idx)
+            return idx, 200
+        except ValueError:
+            return  -5, 404
+
+
+# Adding resources to the Flask app API
+api.add_resource(Dishes, '/dishes')
+api.add_resource(DishesId, '/dishes/<int:idx>')
+api.add_resource(DishesName, '/dishes/<string:name>')
+
+if __name__ == '__main__':
+    print(f"Running Meals&Dishes server ({__file__})")
+    app.run(port=5002)  # TODO: read port from cfg/argsparser
+
---
+++ test.py
@@ -0,0 +1,147 @@
+"""
+Client module for testing
+
+TODO:
+* Typing
+"""
+
+
+import requests
+
+
+# Sources paths
+root = 'http://127.0.0.1:5002'
+dishes = root + '/dishes'
+
+
+# Function for communicating with server
+def get_all_dishes():
+    return requests.get(dishes)
+
+
+def post_dish(name):
+    return requests.post(dishes, params={'name': name})
+
+
+def get_dish_by_name(name):  # TODO: remove this or previous func
+    return requests.get(dishes + '/' + name)
+
+
+def delete_dish_by_name(name: str):
+    return requests.delete(dishes + '/' + name)
+
+
+def get_dish_by_idx(idx):
+    return requests.get(dishes + '/' + idx)
+
+
+def delete_dish_by_idx(idx):
+    return requests.delete(dishes + '/' + idx)
+
+
+# Verification functions
+def print_res(res):
+    print(res.json(), res)
+    print()
+
+
+def verify_res_json(res, json):
+    assert res.json() == json
+
+
+def verify_res_code(res, code):
+    assert res.status_code == code
+
+
+def verify_res(res, json, code):
+    verify_res_json(res, json)
+    verify_res_code(res, code)
+
+
+
+
+
+# Tests
+
+print(f"Test 1#: getting all dishes. Should return empty dict")
+res = get_all_dishes()
+print_res(res)
+verify_res_code(res, 200)
+
+
+print(f"Test 2#: Adding a 'fish' dish. Should return 1 (index of the dish) with code 201")
+res = post_dish('fish')
+print_res(res)
+verify_res_code(res, 201)
+
+
+print(f"Test 3#: Deleting the whole dishes resource. Should give an error with code 405")
+res = requests.delete(dishes)
+print_res(res)
+verify_res_code(res, 405)
+
+
+print(f"Test 4#: Getting dish of index 1. Should return the 'fish' dish")
+res = get_dish_by_idx('1')
+print_res(res)
+verify_res_code(res, 200)
+
+
+print(f"Test 5#: Getting dish 'fish'. Should return the 'fish' dish")
+res = get_dish_by_name('fish')
+print_res(res)
+verify_res_code(res, 200)
+
+
+print(f"Test 6#: Getting dish of index 2. Should return -5 with code 404")
+res = get_dish_by_idx('2')
+print_res(res)
+verify_res_code(res, 404)
+assert res.text.rstrip() == "-5"  # TODO: why do we need rstrip? Why do we have a linebreak in the reponse?
+
+
+print(f"Test 7#: Getting a dish 'phish'. Should return -5 with status 404.")
+res = get_dish_by_name('phish')
+print_res(res)
+verify_res_code(res, 404)
+assert res.text.rstrip() == '-5'
+
+
+print(f"Test 8#: Deleting a dish of index 1. Should return code 200 and value 1 (the index of fish)")
+res = delete_dish_by_idx('1')
+print_res(res)
+verify_res_code(res, 200)
+assert res.text.rstrip() == '1'
+
+
+print(f"Test 9#: Adding and deleting a dish 'chicken'. Should return code 200 and value 2 (the index of chicken)")
+post_dish('chicken')
+res = delete_dish_by_name('chicken')
+print_res(res)
+verify_res_code(res, 200)
+assert res.text.rstrip() == '2'
+
+
+print(f"Test 10#: Dleting a dish of index 3. Should return -5 with status 404.")
+response = delete_dish_by_idx('3')
+print_res(response)
+verify_res_code(response, 404)
+assert response.text.rstrip() == '-5'
+
+
+print(f"Test 11#: Deleting a dish 'phish'. Should return -5 with status 404.")
+response = delete_dish_by_name('phish')
+print_res(response)
+verify_res_code(response, 404)
+assert response.text.rstrip() == '-5'
+
+
+
+
+
+# Tests for Meals
+
+post_dish('salad')
+post_dish('steak')
+post_dish('ice cream')
+print_res(get_all_dishes())
